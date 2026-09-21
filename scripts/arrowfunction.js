19f789c4add895a2ec4ea34606d278a2996c314f
PlayErsGames666 <playersgames.ru@gmail.com>
2026-09-21 17:08:07 +0500

    Create arrowfunction file with new function which has summaraice th number values =>

---
+++ scripts/arrowfunction.js
@@ -0,0 +1,8 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+let result = add(10, 20);
+
+document.getElementById('arrowFunc').innerHTML = "Result of summaraising with arrow function of 10 and 20 is: " + result;
+
